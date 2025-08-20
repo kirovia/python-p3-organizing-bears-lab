6f58dc527239c1b4472ce81dcd204a6fb67e2f5b
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-08-19 22:40:08 -0400

    Passes all tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'F', 'Black', 'Broody', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, 'F', 'Black', 'Lazy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'Brown', 'Adventurous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 14, 'F', 'White', 'Mysterious', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 20, 'M', 'Green', 'Grouchy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 28, 'F', 'White', 'Sweet', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'F', 'Brown', 'Dodgy', 1);
